4128cf6b30256ccdb280a278a5a8f9113bfe068d
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-10 22:19:24 -0500

    commit tree 76314cafd12fd1a24c7757f5980acd715dfb5a27

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(input)
     if input=="I LOVE YOU GRANDMA!"
         return "I LOVE YOU TOO PUMPKIN!"
-    elsif input!=input.upcase
-        return "HUH?! SPEAK UP, SONNY!"
     elsif input==input.upcase
         return "NO, NOT SINCE 1938!"
+    else
+        return "HUH?! SPEAK UP, SONNY!"
     end
 end
 # Whatever you say to grandma, she should respond with
